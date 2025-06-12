c3335227fb21a9a6f5b998fce1ff00a884c5169b
Furkan Can İşci <furkancanisci@outlook.com>
2025-06-12 15:30:02 +0300

    some change

--- entryproject/settings.py
+++ entryproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,16 +104,8 @@ WSGI_APPLICATION = 'entryproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'supabase_db_name',
-        'USER': 'supabase_user',
-        'PASSWORD': 'supabase_password',
-        'HOST': 'db.<projeid>.supabase.co',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 
 
 # Password validation
