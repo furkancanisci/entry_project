ab0e9762ebd0d6c6bce92fa0d23521f302b387ab
Furkan Can İşci <furkancanisci@outlook.com>
2025-11-01 12:21:42 +0300

    databse conf

--- entryproject/settings.py
+++ entryproject/settings.py
@@ -105,16 +105,7 @@ WSGI_APPLICATION = 'entryproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'entryproject',
-        'USER': 'postgres',
-        'PASSWORD': '135720',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
