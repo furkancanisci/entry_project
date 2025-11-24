ce436f28fa9b1ecc71f64f08d315e0b3f0a98a34
Furkan Can İşci <furkancanisci@outlook.com>
2025-11-24 16:11:02 +0300

    chore(settings): update settings.py and reload application on changes
    - Detected modification in settings.py and reloaded application accordingly
    - Enabled file watching for templates and locale directories for auto reload
    - Added monitoring of various Django and third-party package files for changes
    - Triggered autoreload signals upon apps readiness and file changes
    - Improved development workflow with automatic detection and reload of changes

--- entryproject/settings.py
+++ entryproject/settings.py
@@ -106,6 +106,11 @@ WSGI_APPLICATION = 'entryproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'postgres://postgres:135720@localhost:5432/entryproject')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
